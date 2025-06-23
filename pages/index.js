363b14ca418cc98e8ec34d73020c7f2b7c729287
Jenisha <avrj2020@gmail.com>
2025-06-23 16:27:28 +0530

    Updated env variables

--- pages/index.js
+++ pages/index.js
@@ -89,7 +89,7 @@ export default function Home({ stories }) {
 }
 
 export async function getServerSideProps() {
-  const res = await fetch('https://mystree-app-o778.vercel.app/api/stories')
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || ''}/api/stories`)
   const json = await res.json()
 
   return {
